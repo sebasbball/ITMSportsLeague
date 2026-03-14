379e8a2a424bfefe06b15f3c88984b66fb5fd041
Sebastian Zapata <sebas.xapata@gmail.com>
2026-03-13 21:52:23 -0500

    New Entity Player Created *Pending continue num 10 from doc PhaseZ

---
+++ SportsLeague.Domain/Interfaces/Repositories/IPlayerRepository.cs
@@ -0,0 +1,17 @@
+﻿using SportsLeague.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsLeague.Domain.Interfaces.Repositories
+{
+    public interface IPlayerRepository : IGenericRepository<Player>
+    {
+        Task<IEnumerable<Player>> GetByTeamAsync(int teamId);
+        Task<Player?> GetByTeamAndNumberAsync(int teamId, int number);
+        Task<IEnumerable<Player>> GetAllWithTeamAsync();
+        Task<Player?> GetByIdWithTeamAsync(int id);
+    }
+
+
+}
