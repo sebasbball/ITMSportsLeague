6d800ce2d9ab94d78e05fc4571da5423a06e196e
Sebastian Zapata <sebas.xapata@gmail.com>
2026-03-21 17:45:57 -0500

    Updated Phase #2

--- SportsLeague.API/Controllers/TeamController.cs
+++ SportsLeague.API/Controllers/TeamController.cs
@@ -13,16 +13,13 @@ public class TeamController : ControllerBase
 {
     private readonly ITeamService _teamService;
     private readonly IMapper _mapper;
-    private readonly ILogger<TeamController> _logger;
 
     public TeamController(
         ITeamService teamService,
-        IMapper mapper,
-        ILogger<TeamController> logger)
+        IMapper mapper)
     {
         _teamService = teamService; 
         _mapper = mapper;
-        _logger = logger;
     }
 
     [HttpGet]
--- SportsLeague.API/Mappings/MappingProfile.cs
+++ SportsLeague.API/Mappings/MappingProfile.cs
@@ -12,7 +12,15 @@ namespace SportsLeague.API.Mappings
             // Team mappings
             CreateMap<TeamRequestDTO, Team>();
             CreateMap<Team, TeamResponseDTO>();
+
+            // Player mappings
+            CreateMap<PlayerRequestDTO, Player>();
+            CreateMap<Player, PlayerResponseDTO>()
+                .ForMember(
+                    dest => dest.TeamName,
+                    opt => opt.MapFrom(src => src.Team.Name));
         }
     }
+
 
 }
