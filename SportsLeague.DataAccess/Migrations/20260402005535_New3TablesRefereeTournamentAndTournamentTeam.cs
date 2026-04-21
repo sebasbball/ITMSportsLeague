42dd7d8fac9dffb99adc339fb9441531cbf38c9b
Sebastian Zapata <sebas.xapata@gmail.com>
2026-04-21 17:01:08 -0500

    Update

--- SportsLeague.DataAccess/Migrations/20260402005535_New3TablesRefereeTournamentAndTournamentTeam.cs
+++ SportsLeague.DataAccess/Migrations/20260402005535_New3TablesRefereeTournamentAndTournamentTeam.cs
@@ -5,10 +5,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace SportsLeague.DataAccess.Migrations
 {
-    /// <inheritdoc />
     public partial class New3TablesRefereeTournamentAndTournamentTeam : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
