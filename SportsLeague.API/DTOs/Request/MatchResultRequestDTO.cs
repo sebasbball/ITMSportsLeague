8644251e26641d3efe97359287b53ed2444ddc58
Sebastian Zapata <sebas.xapata@gmail.com>
2026-05-12 17:22:01 -0500

    Phase 5 (Second Part) + DTO´s, Mappings, Controllers, Programs.cs, Migrations.

---
+++ SportsLeague.API/DTOs/Request/MatchResultRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SportsLeague.API.DTOs.Request
+{
+    public class MatchResultRequestDTO
+    {
+        public int HomeGoals { get; set; }
+        public int AwayGoals { get; set; }
+        public string? Observations { get; set; }
+    }
+}
--- SportsLeague.API/Program.cs
+++ SportsLeague.API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using SportsLeague.DataAccess.Context;
 using SportsLeague.DataAccess.Repositories;
+using SportsLeague.Domain.Helpers;
 using SportsLeague.Domain.Interfaces.Repositories;
 using SportsLeague.Domain.Interfaces.Services;
 using SportsLeague.Domain.Services;
@@ -20,6 +21,10 @@ builder.Services.AddScoped<IRefereeRepository, RefereeRepository>();
 builder.Services.AddScoped<ITournamentRepository, TournamentRepository>();
 builder.Services.AddScoped<ITournamentTeamRepository, TournamentTeamRepository>();
 builder.Services.AddScoped<IMatchRepository, MatchRepository>();
+builder.Services.AddScoped<IMatchResultRepository, MatchResultRepository>();
+builder.Services.AddScoped<IGoalRepository, GoalRepository>();
+builder.Services.AddScoped<ICardRepository, CardRepository>();
+
 
 // ── Services ──
 builder.Services.AddScoped<ITeamService, TeamService>();
@@ -27,6 +32,9 @@ builder.Services.AddScoped<IPlayerService, PlayerService>();
 builder.Services.AddScoped<IRefereeService, RefereeService>();
 builder.Services.AddScoped<ITournamentService, TournamentService>();
 builder.Services.AddScoped<IMatchService, MatchService>();
+builder.Services.AddScoped<IMatchEventService, MatchEventService>();
+builder.Services.AddScoped<MatchValidationHelper>();
+
 
 // ── AutoMapper ──
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
